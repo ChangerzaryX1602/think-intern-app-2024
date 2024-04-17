e53b4872b595e21631fd43bd5e2ba1824c385d17
ChangerzaryX <chatchanan.pa@kkumail.com>
2024-04-17 16:04:00 +0700

    feat : fetch api and display image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,40 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  
-  <style lang="postcss">
-    :global(html) {
-      background-color: theme(colors.gray.100);
+<script>
+  import axios from "axios";
+  import { onMount } from "svelte";
+  let ImageURL = "";
+  const requestAPI = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.thecatapi.com/v1/images/search"
+      );
+      if (response.data && response.data.length > 0) {
+        ImageURL = response.data[0].url;
+      }
+      console.log("Hi");
+    } catch (error) {
+      console.error(error);
     }
-  </style>
+  };
+  onMount(() => {
+    requestAPI();
+  });
+</script>
+
+<button class="cat" tabindex="0" aria-label="Get a random cat image" on:click={requestAPI}>
+  {#if ImageURL}
+    <img src={ImageURL} alt="random img" />
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</button>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+  .cat{
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+  }
+</style>
